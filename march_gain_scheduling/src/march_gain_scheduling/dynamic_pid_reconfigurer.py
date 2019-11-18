f15813d38665c6e4b04e8ccbe7a8bb7839d3f3ec
rogier <rogierkrijnen@live.nl>
2019-11-18 15:14:01 +0100

    Update client_update method to use linearization and renamed the python file for the linearization according to lowercase _ namespace

--- march_gain_scheduling/src/march_gain_scheduling/dynamic_pid_reconfigurer.py
+++ march_gain_scheduling/src/march_gain_scheduling/dynamic_pid_reconfigurer.py
@@ -2,7 +2,7 @@
 import rospy
 from dynamic_reconfigure.client import Client
 from march_shared_resources.msg import GaitNameActionGoal
-
+from interpolate_pid_values import interpolate
 
 class DynamicPIDReconfigurer:
     def __init__(self, gait_name=None, joint_list=None):
@@ -12,6 +12,7 @@ class DynamicPIDReconfigurer:
         for i in range(len(self._joint_list)):
             self._clients.append(Client("/march/controller/trajectory/gains/" + self._joint_list[i], timeout=30))
         rospy.Subscriber("/march/gait/schedule/goal", GaitNameActionGoal, callback=self.gait_selection_callback)
+        self._linearize = rospy.get_param("~Linearize_gain_scheduling")
 
     def gait_selection_callback(self, data):
         rospy.logdebug("This is the gait name: %s", data.goal.current_subgait.gait_type)
@@ -22,13 +23,23 @@ class DynamicPIDReconfigurer:
             self.client_update()
 
     def client_update(self):
-        for i in range(len(self._joint_list)):
-            p_value, i_value, d_value = self.look_up_table(i)
-            if p_value is not None:
-                self._clients[i].update_configuration({"p": p_value,
-                                                       "i": i_value,
-                                                       "d": d_value})
-                rospy.logdebug("Config set to {0}, {1}, {2}".format(p_value, i_value, d_value))
+        if self._linearize:
+            for i in range(len(self._joint_list)):
+                p_value, i_value, d_value = self.look_up_table(i)
+                p_value_list = interpolate(p_value, self._clients[i].get_configuration()[0])
+                for k in range(len(p_value_list)):
+                    self._clients[i].update_configuration({"p": p_value_list[k],
+                                                           "i": i_value,
+                                                           "d": d_value})
+                    rospy.logdebug("Config set to {0}, {1}, {2}".format(p_value, i_value, d_value))
+        else:
+            for i in range(len(self._joint_list)):
+                p_value, i_value, d_value = self.look_up_table(i)
+                if p_value is not None:
+                    self._clients[i].update_configuration({"p": p_value,
+                                                           "i": i_value,
+                                                           "d": d_value})
+                    rospy.logdebug("Config set to {0}, {1}, {2}".format(p_value, i_value, d_value))
 
     # Method that pulls the PID values from the gains_per_gait_type.yaml config file
     def look_up_table(self, i):
