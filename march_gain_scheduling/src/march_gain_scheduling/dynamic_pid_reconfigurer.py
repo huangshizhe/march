f6e6efa7909629139898298b618d66d6ca61902c
rogier <rogierkrijnen@live.nl>
2019-11-19 17:25:22 +0100

    Change _type to _like in config file and adjust subscriber topics in class file.

--- march_gain_scheduling/src/march_gain_scheduling/dynamic_pid_reconfigurer.py
+++ march_gain_scheduling/src/march_gain_scheduling/dynamic_pid_reconfigurer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import rospy
 from dynamic_reconfigure.client import Client
-from march_shared_resources.msg import GaitNameActionGoal
+from march_shared_resources.msg import GaitActionGoal
 from interpolate_pid_values import interpolate
 
 class DynamicPIDReconfigurer:
@@ -11,18 +11,19 @@ class DynamicPIDReconfigurer:
         self._clients = []
         for i in range(len(self._joint_list)):
             self._clients.append(Client("/march/controller/trajectory/gains/" + self._joint_list[i], timeout=30))
-        rospy.Subscriber("/march/gait/schedule/goal", GaitNameActionGoal, callback=self.gait_selection_callback)
-        self._linearize = rospy.get_param("~Linearize_gain_scheduling")
+        rospy.Subscriber("/march/gait/schedule/goal", GaitActionGoal, callback=self.gait_selection_callback)
+        self._linearize = rospy.get_param("/linearize_gain_scheduling")
 
     def gait_selection_callback(self, data):
         rospy.logdebug("This is the gait name: %s", data.goal.current_subgait.gait_type)
-        if self._gait_name != data.goal.name:
+        if self._gait_name != data.goal.current_subgait.gait_type:
             rospy.logdebug("The selected gait: {0} is not the same as the previous gait: {1}".format(
                 data.goal.current_subgait.gait_type, self._gait_name))
             self._gait_name = data.goal.current_subgait.gait_type
             self.client_update()
 
     def client_update(self):
+        rospy.logdebug("self.linearize is: {0}".format(self._linearize))
         if self._linearize:
             for i in range(len(self._joint_list)):
                 p_value, i_value, d_value = self.look_up_table(i)
@@ -39,12 +40,12 @@ class DynamicPIDReconfigurer:
                     self._clients[i].update_configuration({"p": p_value,
                                                            "i": i_value,
                                                            "d": d_value})
-                    rospy.logdebug("Config set to {0}, {1}, {2}".format(p_value, i_value, d_value))
+                    rospy.logdebug("nonlinear Config set to {0}, {1}, {2}".format(p_value, i_value, d_value))
 
     # Method that pulls the PID values from the gains_per_gait_type.yaml config file
     def look_up_table(self, i):
-        if rospy.has_param("~gait_types/" + self._gait_name + "_gains/"):
-            gains = rospy.get_param("~gait_types/" + self._gait_name + "_gains/" + self._joint_list[i])
+        if rospy.has_param("~gait_types/" + self._gait_name):
+            gains = rospy.get_param("~gait_types/" + self._gait_name + "/" + self._joint_list[i])
             return gains['p'], gains['i'], gains['d']
         else:
             return None, None, None
