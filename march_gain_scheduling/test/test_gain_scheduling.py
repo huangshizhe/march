c84834d1517065268f7307dcdd405b55692def6b
rogier <rogierkrijnen@live.nl>
2019-11-27 15:09:35 +0100

    change nodetest to initialize node

--- march_gain_scheduling/test/test_gain_scheduling.py
+++ march_gain_scheduling/test/test_gain_scheduling.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import unittest
+import rospy
 from march_gain_scheduling.dynamic_pid_reconfigurer import DynamicPIDReconfigurer
 
 PKG = 'march_gain_scheduling'
@@ -7,7 +8,10 @@ PKG = 'march_gain_scheduling'
 
 class GainSchedulingTest(unittest.TestCase):
 
-    def test_a(self):
+    def test_empty_object(self):
+        # Load the "/march/controller/trajectory/gains/" + self._joint_list[i] parameters
+        rospy.init_node("gainschedulingtest")
+
         self.assertEqual(1, 1, "1!=1")
 
 
