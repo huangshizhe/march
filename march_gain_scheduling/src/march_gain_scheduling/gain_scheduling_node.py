fd59e3f3b3546c92e09715c3cbd4743fff45ad99
rogier <rogierkrijnen@live.nl>
2019-11-28 10:34:33 +0100

    wip fixbeun al die dingen

--- march_gain_scheduling/src/march_gain_scheduling/gain_scheduling_node.py
+++ march_gain_scheduling/src/march_gain_scheduling/gain_scheduling_node.py
@@ -4,10 +4,8 @@ from dynamic_pid_reconfigurer import DynamicPIDReconfigurer
 
 
 def main():
+    rospy.init_node("march_gain_scheduling_node")
     # The parameters Joint_list and gait_list should be obtained from the parameter server of the workspace.
     joint_list = rospy.get_param("/march/joint_names")
-
     DynamicPIDReconfigurer("random", joint_list)
-
-    rospy.init_node("march_gain_scheduling_node")
     rospy.spin()
--- march_gain_scheduling/test/rostest/gain_scheduling_test.py
+++ march_gain_scheduling/test/rostest/gain_scheduling_test.py
@@ -9,28 +9,36 @@ PKG = 'march_gain_scheduling'
 
 class GainSchedulingTest(unittest.TestCase):
     # Load the "/march/controller/trajectory/gains/" + self._joint_list[i] parameters
-    def __init__(self):
-        self.gait_name = ""
-        self.subgait = ""
+    # def __init__(self):
+    #     self.gait_name = ""
+    #     self.subgait = ""
+    #
+    #     # Fix beun de action goal met een publisher zodat je niet op antwoord hoeft te wachten.
+    #     self.pub = rospy.publisher("/march/gait/schedule/goal", GaitActionGoal, queue_size=1)
+    #     rospy.init_node("gainschedulingtest")
+    #     self.gait_action_goal = GaitGoal()
+    #     self.gait_action_goal.name = self.gait_name
+    #     self.gait_action_goal.current_subgait = self.subgait
+    #     self.pub.publish(self.gait_action_goal)
+    #
+    # def test_empty_object_for_joint_list(self):
+    #     dynamic_pid_reconfigurer = DynamicPIDReconfigurer()
+    #     self.assertEqual(dynamic_pid_reconfigurer._joint_list, [], "empty object=empty joint list")
+    #
+    # def test_empty_object_for_linearization(self):
+    #     dynamic_pid_reconfigurer = DynamicPIDReconfigurer()
+    #     lin = dynamic_pid_reconfigurer._linearize
+    #     self.assertEqual(rospy.get_param("/linearize_gain_scheduling"), lin, "linearize param is set correctly")
 
-        # Fix beun de action goal met een publisher zodat je niet op antwoord hoeft te wachten.
-        self.pub = rospy.publisher("/march/gait/schedule/goal", GaitActionGoal, queue_size=1)
-        rospy.init_node("gainschedulingtest")
-        self.gait_action_goal = GaitGoal()
-        self.gait_action_goal.name = self.gait_name
-        self.gait_action_goal.current_subgait = self.subgait
-        self.pub.publish(self.gait_action_goal)
-
-    def test_empty_object_for_joint_list(self):
-        dynamic_pid_reconfigurer = DynamicPIDReconfigurer()
-        self.assertEqual(dynamic_pid_reconfigurer._joint_list, [], "empty object=empty joint list")
-
-    def test_empty_object_for_linearization(self):
-        dynamic_pid_reconfigurer = DynamicPIDReconfigurer()
-        lin = dynamic_pid_reconfigurer._linearize
-        self.assertEqual(rospy.get_param("/linearize_gain_scheduling"), lin, "linearize param is set correctly")
+    def test_subscription(self):
+        pub = rospy.publisher("/march/gait/schedule/goal", GaitActionGoal, queue_size=1)
+        end_time = rospy.get_rostime() + rospy.Duration(5)
+        while pub.get_num_connections() == 0 and rospy.get_rostime() < end_time:
+            rospy.sleep(0.1)
+        self.assertEqual(pub.get_num_connections(), 1)
 
 
 if __name__ == '__main__':
-    import src.march.march_gain_scheduling.test.rostest
-    src.march.march_gain_scheduling.test.rostest.rosrun(PKG, 'test_gain_scheduling', GainSchedulingTest)
+    import rostest
+    rospy.init_node("gainschedulingtest")
+    rostest.rosrun(PKG, 'test_gain_scheduling', GainSchedulingTest)
