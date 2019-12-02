b435a297b11fd7414f5eca85e261c29cafc8e35e
rogier <rogierkrijnen@live.nl>
2019-12-02 14:41:27 +0100

    Add init and second test that checks dynamic reconfigurability

--- march_gain_scheduling/test/rostest/gain_scheduling_test.py
+++ march_gain_scheduling/test/rostest/gain_scheduling_test.py
@@ -4,6 +4,7 @@ import rospy
 
 from march_shared_resources.msg import GaitActionGoal, GaitGoal
 from march_gain_scheduling.dynamic_pid_reconfigurer import DynamicPIDReconfigurer
+from dynamic_reconfigure.client import Client
 from dynamic_reconfigure.server import Server
 from march_gain_scheduling.cfg import GainListConfig
 
@@ -17,17 +18,6 @@ def callback(config, level):
 
 class GainSchedulingTest(unittest.TestCase):
     # Load the "/march/controller/trajectory/gains/" + self._joint_list[i] parameters
-    # def __init__(self):
-    #     self.gait_name = ""
-    #     self.subgait = ""
-    #
-    #     # Fix beun de action goal met een publisher zodat je niet op antwoord hoeft te wachten.
-    #     self.pub = rospy.publisher("/march/gait/schedule/goal", GaitActionGoal, queue_size=1)
-    #     rospy.init_node("gainschedulingtest")
-    #     self.gait_action_goal = GaitGoal()
-    #     self.gait_action_goal.name = self.gait_name
-    #     self.gait_action_goal.current_subgait = self.subgait
-    #     self.pub.publish(self.gait_action_goal)
     #
     # def test_empty_object_for_joint_list(self):
     #     dynamic_pid_reconfigurer = DynamicPIDReconfigurer()
@@ -38,8 +28,16 @@ class GainSchedulingTest(unittest.TestCase):
     #     lin = dynamic_pid_reconfigurer._linearize
     #     self.assertEqual(rospy.get_param("/linearize_gain_scheduling"), lin, "linearize param is set correctly")
 
+    def __init__(self, *args, **kwargs):
+        super(GainSchedulingTest, self).__init__(*args, **kwargs)
+        self._client = Client("/march/controller/trajectory/gains/test_joint1", timeout=30)
+
     def setUp(self):
+        self._client.update_configuration({"p": 1000, "i": 0, "d": 10})
         return True
+
+    def tearDown(self):
+        self._client.close()
 
     def test_subscription(self):
         pub = rospy.Publisher('/march/gait/schedule/goal', GaitActionGoal, queue_size=1)
@@ -49,7 +47,12 @@ class GainSchedulingTest(unittest.TestCase):
         self.assertEqual(pub.get_num_connections(), 1, "No connections")
 
     def test_reconfigured_pid_values(self):
-        self.assertTrue(True)
+        old_val = self._client.get_configuration()
+        new_val = {"p": 1001, "i": 0, "d": 10}
+        self._client.update_configuration(new_val)
+        updated_val = self._client.get_configuration()
+        self.assertEqual(old_val['p']+1, updated_val['p'], "Client configuration did not update as expected old_val = \
+        {0} and updated_val = {1}".format(old_val['p'], updated_val['p']))
 
 
 if __name__ == '__main__':
