a982542abd9a135f1ecba0ca88c300ae36ad87a4
rogier <rogierkrijnen@live.nl>
2019-12-06 11:12:31 +0100

    fix

--- march_gain_scheduling/src/march_gain_scheduling/gain_scheduling_node.py
+++ march_gain_scheduling/src/march_gain_scheduling/gain_scheduling_node.py
@@ -1,11 +1,12 @@
-#!/usr/bin/env python
-from dynamic_pid_reconfigurer import DynamicPIDReconfigurer
 import rospy
+
+from .dynamic_pid_reconfigurer import DynamicPIDReconfigurer
 
 
 def main():
-    rospy.init_node('march_gain_scheduling_node')
     # The parameters Joint_list and gait_list should be obtained from the parameter server of the workspace.
+    rospy.init_node('march_gain_scheduling_node')
     joint_list = rospy.get_param('/march/joint_names')
-    DynamicPIDReconfigurer('random', joint_list)
+    DynamicPIDReconfigurer(joint_list=joint_list)
+
     rospy.spin()
